c2ff60922a9fd83f87f84c54c12c33fae3e65b2a
a-rasines <alberto.rasines@opendeusto.es>
2022-10-07 11:53:40 +0200

    Movimiento segun hijos

--- Assets/Scripts/RaftTurbulence.cs
+++ Assets/Scripts/RaftTurbulence.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class RaftTurbulence : MonoBehaviour
 {
     void Start(){
-      for(int i = 0; i < transform.childCount; i++) {
+        calculateMotion();
+    }
+    private void calculateMotion() {
+        childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++) {
             Transform t = transform.GetChild(i);
-            if(t.localPosition.x + t.lossyScale.x/2 > maxX) {
+            if (t.localPosition.x + t.lossyScale.x / 2 > maxX) {
                 maxX = t.localPosition.x + t.lossyScale.x / 2;
-            }else if(t.localPosition.x - t.lossyScale.x / 2 < minX) {
+            } else if (t.localPosition.x - t.lossyScale.x / 2 < minX) {
                 minX = t.localPosition.x - t.lossyScale.x / 2;
             }
             if (t.localPosition.y + t.lossyScale.y / 2 > maxY) {
@@ -17,7 +21,7 @@ public class RaftTurbulence : MonoBehaviour
             } else if (t.localPosition.y - t.lossyScale.y / 2 < minY) {
                 minY = t.localPosition.y - t.lossyScale.y / 2;
             }
-        }  
+        }
     }
     private float rotation = 0;
     public float speed = 1;
@@ -28,11 +32,13 @@ public class RaftTurbulence : MonoBehaviour
     private float maxY;
     private float minX;
     private float minY;
+    private int childCount;
     void Update(){
+        if (childCount != transform.childCount) calculateMotion();
         rotation += speed * Time.deltaTime;
         float height = Mathf.Sin(rotation) * strenght / 2;
-        print(height);
-        float angle = height==0 || maxX == minX ? 0 : Mathf.Atan((float)(height / Mathf.Tan((maxY - minY)/(maxX - minX)))/*Cambiar por el tamaño de la balsa*/) * Mathf.Rad2Deg;
+        print(""+height + " (" + maxX + ":"+maxY+") ("+minX+":"+minY+")");
+        float angle = height==0 || maxX == minX ? 0 : Mathf.Atan((float)(height / Mathf.Sqrt(Mathf.Pow(maxY - minY,2) + Mathf.Pow(maxX - minX, 2)))/*Cambiar por el tamaño de la balsa*/) * Mathf.Rad2Deg;
         transform.rotation= Quaternion.Euler(x * angle, 0, z * angle);
 
     }
