7e087e66e298f4950a3044148f56381dabd2cd43
a-rasines <alberto.rasines@opendeusto.es>
2022-10-07 11:38:35 +0200

    Intento de que cuando crezca la balsa baje el movimiento

--- Assets/Scripts/RaftTurbulence.cs
+++ Assets/Scripts/RaftTurbulence.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class RaftTurbulence : MonoBehaviour
 {
     void Start(){
-        
+      for(int i = 0; i < transform.childCount; i++) {
+            Transform t = transform.GetChild(i);
+            if(t.localPosition.x + t.lossyScale.x/2 > maxX) {
+                maxX = t.localPosition.x + t.lossyScale.x / 2;
+            }else if(t.localPosition.x - t.lossyScale.x / 2 < minX) {
+                minX = t.localPosition.x - t.lossyScale.x / 2;
+            }
+            if (t.localPosition.y + t.lossyScale.y / 2 > maxY) {
+                maxY = t.localPosition.y + t.lossyScale.y / 2;
+            } else if (t.localPosition.y - t.lossyScale.y / 2 < minY) {
+                minY = t.localPosition.y - t.lossyScale.y / 2;
+            }
+        }  
     }
     private float rotation = 0;
     public float speed = 1;
     public float strenght = 1;
     public float x = 1;
     public float z = 1;
+    private float maxX;
+    private float maxY;
+    private float minX;
+    private float minY;
     void Update(){
         rotation += speed * Time.deltaTime;
         float height = Mathf.Sin(rotation) * strenght / 2;
         print(height);
-        float angle = height==0? 0 : Mathf.Atan((float)(height / 0.5)/*Cambiar por el tamaño de la balsa*/) * Mathf.Rad2Deg;
+        float angle = height==0 || maxX == minX ? 0 : Mathf.Atan((float)(height / Mathf.Tan((maxY - minY)/(maxX - minX)))/*Cambiar por el tamaño de la balsa*/) * Mathf.Rad2Deg;
         transform.rotation= Quaternion.Euler(x * angle, 0, z * angle);
 
     }
