593b9c5963d0d63e7bfa682ce009ff2f69b1a359
a-rasines <alberto.rasines@opendeusto.es>
2023-01-09 14:42:38 +0100

    Cuerda, faltan arreglos

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -1,15 +1,26 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Hook : MonoBehaviour {
     // Start is called before the first frame update
+    public Transform start;
+    public Transform end;
+    public MeshFilter rope;
+    private Vector3 startPosition;
+    private Vector3 startRotation;
+    private Vector3 endPosition;
+    private Vector3 endRotation;
+    public int vertex = 8;
+    public float thickness = 0.5f;
     private bool thrown = false;
     private float startTime = 0;
     void Start() {
-
-        
+        startPosition = start.position;
+        startRotation = start.eulerAngles;
+        endPosition = end.position;
+        endRotation = end.eulerAngles;
     }
     private void OnTriggerEnter(Collider other) {
         if (thrown && other.gameObject.tag == "Grabable")
@@ -17,6 +28,7 @@ public class Hook : MonoBehaviour {
     }
     public void OnGrab() {
         thrown = false;
+        rope.mesh = null;
         GetComponent<Collider>().isTrigger = true;
     }
     public void OnGrabEnd() {
@@ -38,6 +50,8 @@ public class Hook : MonoBehaviour {
             GetComponent<XRGrabInteractable>().enabled = false;
             StartCoroutine(ResetGrabable());
             thrown = true;
+            rope.mesh = new Mesh();
+            CreateRope();
         }
 
     }
@@ -46,10 +60,78 @@ public class Hook : MonoBehaviour {
         yield return new WaitForSeconds(1);
         GetComponent<XRGrabInteractable>().enabled = true;
     }
+    int[] triangles = new int[0];
+    private void CreateRope() {
+        startPosition = start.position;
+        startRotation = start.eulerAngles;
+        endPosition = end.position;
+        endRotation = end.eulerAngles;
+        float step = 2 * Mathf.PI / vertex;
+        Vector3[] vertices = new Vector3[vertex * 2 + 2];
+        vertices[0] = startPosition - new Vector3(transform.position.x, 0, transform.position.z);
+        for (int i = 1; i < vertex + 1; i++) {//Start
+            //(Sp + cos(a), Sp + sen(a), S+cos(Sr))
+            vertices[i] = new Vector3(startPosition.x - transform.position.x + Mathf.Cos(step * (i - 1)) * thickness, startPosition.y + Mathf.Sin(step * (i - 1)) * thickness, startPosition.z - transform.position.z + Mathf.Cos(startRotation.y) * Mathf.Cos(step * (i - 1)) * thickness) ;
+        }
+        for (int i = vertex + 1; i < 2 * vertex + 1; i++) {//End
+            vertices[i] = new Vector3(endPosition.x - transform.position.x + Mathf.Cos(step * (i - 1)) * thickness, endPosition.y + Mathf.Sin(step * (i - 1)) * thickness, endPosition.z - transform.position.z + Mathf.Cos(endRotation.y) * Mathf.Cos(step * (i - 1)) * thickness);
+        }
+        vertices[2 * vertex + 1] = endPosition - new Vector3(transform.position.x, 0, transform.position.z);
+        rope.mesh.vertices = vertices;
+        if (triangles.Length != 12 * vertex) {//3 * vertices + 6 * vertices + 3 * vertices
+            triangles = new int[12 * vertex];
+            int trianglesIndex = 0;
+            //Cara 1
+            for(int i = 1; i < vertex; i++) {
+                triangles[trianglesIndex++] = i;
+                triangles[trianglesIndex++] = 0;
+                triangles[trianglesIndex++] = i + 1;
+            }
+            triangles[trianglesIndex++] = 0;
+            triangles[trianglesIndex++] = 1;
+            triangles[trianglesIndex++] = vertex;
+            
+            //Lados
+            for(int i = 1; i < vertex; i++) {
+                triangles[trianglesIndex++] = i;
+                triangles[trianglesIndex++] = i + 1;
+                triangles[trianglesIndex++] = i + vertex + 1;
+                /*
+                 * 123--
+                 * 132
+                 * 213
+                 * 231
+                 * 312
+                 * 321
+                 */
+                triangles[trianglesIndex++] = i;
+                triangles[trianglesIndex++] = i + vertex + 1;
+                triangles[trianglesIndex++] = i + vertex;
+            }
+            triangles[trianglesIndex++] = vertex;
+            triangles[trianglesIndex++] = 1;
+            triangles[trianglesIndex++] = vertex + 1;
 
+            triangles[trianglesIndex++] = vertex + 1;
+            triangles[trianglesIndex++] = vertex * 2;
+            triangles[trianglesIndex++] = vertex;
+            //Cara 2
+            for (int i = vertex + 1; i < 2 * vertex; i++) {
+                triangles[trianglesIndex++] = 2 * vertex + 1;
+                triangles[trianglesIndex++] = i;
+                triangles[trianglesIndex++] = i + 1;
+            }
+            triangles[trianglesIndex++] = vertex + 1;
+            triangles[trianglesIndex++] = 2 * vertex + 1;
+            triangles[trianglesIndex++] = 2 * vertex;
+            rope.mesh.triangles = triangles;
+        }
+    }
     // Update is called once per frame
     void Update() {
-
+        if(thrown && (start.position != startPosition || start.eulerAngles != startRotation || endPosition != end.position || endRotation != end.eulerAngles)) {
+            CreateRope();
+        }
 
     }
 }
