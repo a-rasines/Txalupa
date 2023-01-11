64f8308b2075f47ceba6f8ded037a66946a0cabd
a-rasines <alberto.rasines@opendeusto.es>
2023-01-11 18:54:57 +0100

    Arreglado gancho, falta retraerlo

--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -46,11 +46,10 @@ public class Hook : MonoBehaviour {
             float time = Mathf.Min(3, Time.time - startTime);
             Rigidbody rb = GetComponent<Rigidbody>();
             rb.isKinematic = false;
-            rb.AddForce(transform.forward * time * 100 );
+            rb.AddForce(transform.forward * time * 1000 );
             GetComponent<XRGrabInteractable>().enabled = false;
             StartCoroutine(ResetGrabable());
             thrown = true;
-            rope.mesh = new Mesh();
             CreateRope();
         }
 
@@ -61,6 +60,7 @@ public class Hook : MonoBehaviour {
         GetComponent<XRGrabInteractable>().enabled = true;
     }
     int[] triangles = new int[0];
+    float offsetY = -0.17f; //Funciona con este valor y no se por qué
     private void CreateRope() {
         startPosition = start.position;
         startRotation = start.eulerAngles;
@@ -68,16 +68,17 @@ public class Hook : MonoBehaviour {
         endRotation = end.eulerAngles;
         float step = 2 * Mathf.PI / vertex;
         Vector3[] vertices = new Vector3[vertex * 2 + 2];
-        vertices[0] = startPosition - new Vector3(transform.position.x, 0, transform.position.z);
+        vertices[0] = startPosition - new Vector3(transform.position.x, transform.position.y + offsetY, transform.position.z);
         for (int i = 1; i < vertex + 1; i++) {//Start
             //(Sp + cos(a), Sp + sen(a), S+cos(Sr))
-            vertices[i] = new Vector3(startPosition.x - transform.position.x + Mathf.Cos(step * (i - 1)) * thickness, startPosition.y + Mathf.Sin(step * (i - 1)) * thickness, startPosition.z - transform.position.z + Mathf.Cos(startRotation.y) * Mathf.Cos(step * (i - 1)) * thickness) ;
+            vertices[i] = new Vector3(startPosition.x - transform.position.x + Mathf.Cos(step * (i - 1)) * thickness, startPosition.y - transform.position.y - offsetY + Mathf.Sin(step * (i - 1)) * thickness, startPosition.z - transform.position.z + Mathf.Cos(startRotation.y) * Mathf.Cos(step * (i - 1)) * thickness) ;
         }
         for (int i = vertex + 1; i < 2 * vertex + 1; i++) {//End
-            vertices[i] = new Vector3(endPosition.x - transform.position.x + Mathf.Cos(step * (i - 1)) * thickness, endPosition.y + Mathf.Sin(step * (i - 1)) * thickness, endPosition.z - transform.position.z + Mathf.Cos(endRotation.y) * Mathf.Cos(step * (i - 1)) * thickness);
+            vertices[i] = new Vector3(endPosition.x - transform.position.x + Mathf.Cos(step * (i - 1)) * thickness, endPosition.y - transform.position.y - offsetY + Mathf.Sin(step * (i - 1)) * thickness, endPosition.z - transform.position.z + Mathf.Cos(endRotation.y) * Mathf.Cos(step * (i - 1)) * thickness);
         }
-        vertices[2 * vertex + 1] = endPosition - new Vector3(transform.position.x, 0, transform.position.z);
+        vertices[2 * vertex + 1] = endPosition - new Vector3(transform.position.x, transform.position.y + offsetY, transform.position.z);
         rope.mesh.vertices = vertices;
+        rope.mesh.RecalculateNormals();
         if (triangles.Length != 12 * vertex) {//3 * vertices + 6 * vertices + 3 * vertices
             triangles = new int[12 * vertex];
             int trianglesIndex = 0;
