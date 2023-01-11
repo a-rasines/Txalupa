852f7efd3311c49b8aa6715e3ad4b26bf0cdb11e
MarianAngulo <81021160+MarianAngulo@users.noreply.github.com>
2023-01-11 21:16:29 +0100

    Muerte

--- Assets/Scripts/PlayerBehaivour.cs
+++ Assets/Scripts/PlayerBehaivour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehaivour : MonoBehaviour
 {
@@ -22,7 +23,7 @@ public class PlayerBehaivour : MonoBehaviour
     {
         if (vida <= 0)
         {
-
+            SceneManager.LoadScene("MuerteScene");
         }
         if(agua)
         {
