7846bca70ae7534c3100e4484d42d45bd0ddf4c3
a-rasines <alberto.rasines@opendeusto.es>
2023-02-11 12:54:44 +0100

    Cambios sin guardar

--- Assets/Scripts/PartiallyChild.cs
+++ Assets/Scripts/PartiallyChild.cs
@@ -21,15 +21,31 @@ public class PartiallyChild : MonoBehaviour {
     public void Recalculate() {
         Vector3 v1 = transform.eulerAngles;
         Vector3 v2 = parent.eulerAngles;
-        dir = new Vector3(exactDirection.x ? 0 : (v1.x - v2.x), exactDirection.y ? 0 : (v1.y - v2.y), exactDirection.z ? 0 : (v1.z - v2.z));
+        dir = new Vector3(
+            (exactDirection.x || !direction.x) ? 0 : (v1.x - v2.x), 
+            (exactDirection.y || !direction.y) ? 0 : (v1.y - v2.y), 
+            (exactDirection.z|| !direction.z) ? 0 : (v1.z - v2.z)
+        );
         v1 = transform.position;
         v2 = parent.position;
-        pos = new Vector3(exactPosition.x ? 0 : (v1.x - v2.x), exactPosition.y ? 0 : (v1.y - v2.y), exactPosition.z ? 0 : (v1.z - v2.z));
+        pos = new Vector3(
+            (exactPosition.x || !position.x) ? 0 : (v1.x - v2.x), 
+            (exactPosition.y || !position.y) ? 0 : (v1.y - v2.y), 
+            (exactPosition.z || !position.z) ? 0 : (v1.z - v2.z)
+        );
     }
     // Update is called once per frame
     void Update() {
-        transform.eulerAngles = new Vector3(direction.x?parent.eulerAngles.x:transform.eulerAngles.x, direction.y ? parent.eulerAngles.y : transform.eulerAngles.y, direction.z ? parent.eulerAngles.z : transform.eulerAngles.z) + pos;
-        transform.position = new Vector3(position.x ? parent.position.x : transform.position.x, position.y ? parent.position.y : transform.position.y, position.z ? parent.position.z : transform.position.z) + pos;
+        transform.eulerAngles = new Vector3(
+            direction.x?parent.eulerAngles.x:transform.eulerAngles.x, 
+            direction.y ? parent.eulerAngles.y : transform.eulerAngles.y, 
+            direction.z ? parent.eulerAngles.z : transform.eulerAngles.z
+            ) + pos;
+        transform.position = new Vector3(
+            position.x ? parent.position.x : transform.position.x, 
+            position.y ? parent.position.y : transform.position.y, 
+            position.z ? parent.position.z : transform.position.z
+            ) + pos;
 
     }
 }
