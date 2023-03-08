c183ded6bf18fa3fe179621ae8ee649d9d4671da
a-rasines <alberto.rasines@opendeusto.es>
2023-03-08 15:18:23 +0100

    Mejora en GUIs (falta pulir)

--- Assets/Scripts/PartiallyChild.cs
+++ Assets/Scripts/PartiallyChild.cs
@@ -11,83 +11,69 @@ public class PartiallyChild : MonoBehaviour {
     public bool3 direction;
     //public bool3 exactDirection;
     public Vector3 maxDirOffset;
-    public bool realTimeDirection;
     public bool3 position;
     //public bool3 exactPosition;
     public Vector3 maxPosOffset;
-    public bool realTimePosition;
     private Vector3 dir;
     private Vector3 pos;
     // Start is called before the first frame update
     void Start() {
-        RecalculatePosition();
-        RecalculateDirecition();
-
-    }
-    public void RecalculateDirecition() {
-        Vector3 v1 = transform.eulerAngles;
-        Vector3 v2 = parent.eulerAngles;
-        dir = new Vector3(
-            direction.x ?
-                maxDirOffset.x < 0 ?
-                    (v1.x - v2.x)
-                : Mathf.Clamp(v1.x - v2.x, -maxDirOffset.x, maxDirOffset.x)
-            : 0,
-            direction.y ?
-                maxDirOffset.y < 0 ?
-                    (v1.y - v2.y)
-                : Mathf.Clamp(v1.y - v2.y, -maxDirOffset.y, maxDirOffset.y)
-            : 0,
-            direction.z ?
-                maxDirOffset.z < 0 ?
-                    (v1.z - v2.z)
-                : Mathf.Clamp(v1.z - v2.z, -maxDirOffset.z, maxDirOffset.z)
-            : 0
-        //(exactDirection.x || !direction.x) ? 0 : (v1.x - v2.x), 
-        //(exactDirection.y || !direction.y) ? 0 : (v1.y - v2.y), 
-        //(exactDirection.z || !direction.z) ? 0 : (v1.z - v2.z)
-        );
-    }
-    public void RecalculatePosition() {
-        Vector3 v1 = transform.position;
-        Vector3 v2 = parent.position;
-        pos = new Vector3(
-            position.x ?
-                maxPosOffset.x < 0 ?
-                    (v1.x - v2.x)
-                : Mathf.Clamp(v1.x - v2.x, -maxPosOffset.x, maxPosOffset.x)
-            : 0,
-            position.y ?
-                maxPosOffset.y < 0 ?
-                    (v1.y - v2.y)
-                : Mathf.Clamp(v1.y - v2.y, -maxPosOffset.y, maxPosOffset.y)
-            : 0,
-            position.z ?
-                maxPosOffset.z < 0 ?
-                    (v1.z - v2.z)
-                : Mathf.Clamp(v1.z - v2.z, -maxPosOffset.z, maxPosOffset.z)
-            : 0
-            //(exactPosition.x || !position.x) ? 0 : (v1.x - v2.x), 
-            //(exactPosition.y || !position.y) ? 0 : (v1.y - v2.y), 
-            //(exactPosition.z || !position.z) ? 0 : (v1.z - v2.z)
-        );
+        dir = transform.eulerAngles - parent.eulerAngles;
+        pos = transform.position - parent.position;
     }
     // Update is called once per frame
     void Update() {
-        if (realTimeDirection)
-            RecalculateDirecition();
-        if (realTimePosition)
-            RecalculatePosition();
-        transform.eulerAngles = new Vector3(
+        /*transform.eulerAngles = new Vector3(
             direction.x?parent.eulerAngles.x:transform.eulerAngles.x, 
             direction.y ? parent.eulerAngles.y : transform.eulerAngles.y, 
             direction.z ? parent.eulerAngles.z : transform.eulerAngles.z
-            ) + pos;
+            ) + pos;*/
         transform.position = new Vector3(
-            position.x ? parent.position.x : transform.position.x, 
-            position.y ? parent.position.y : transform.position.y, 
-            position.z ? parent.position.z : transform.position.z
-            ) + pos;
+            position.x ?
+                maxPosOffset.x < 0 ?
+                parent.position.x + pos.x
+                : transform.position.x - parent.position.x < -maxPosOffset.x || transform.position.x - parent.position.x > maxPosOffset.x?
+                parent.position.x + Math.Clamp(transform.position.x - parent.position.x, -maxPosOffset.x, maxPosOffset.x)
+                : transform.position.x
+            : transform.position.x,
+            position.y ?
+                maxPosOffset.y < 0 ?
+                parent.position.y + pos.y
+                : transform.position.y - parent.position.y < -maxPosOffset.y || transform.position.y - parent.position.y > maxPosOffset.y ?
+                parent.position.y + Math.Clamp(transform.position.y - parent.position.y, -maxPosOffset.y, maxPosOffset.y)
+                : transform.position.y
+            : transform.position.y,
+            position.z ?
+                maxPosOffset.z < 0 ?
+                parent.position.z + pos.z
+                : transform.position.z - parent.position.z < -maxPosOffset.z || transform.position.z - parent.position.z > maxPosOffset.z ?
+                parent.position.z + Math.Clamp(transform.position.z - parent.position.z, -maxPosOffset.z, maxPosOffset.z)
+                : transform.position.z
+            : transform.position.z
+            );
+        transform.eulerAngles = new Vector3(
+            direction.x ?
+                maxDirOffset.x < 0 ?
+                parent.eulerAngles.x + dir.x
+                : transform.eulerAngles.x - parent.eulerAngles.x < -maxDirOffset.x || transform.eulerAngles.x - parent.eulerAngles.x > maxDirOffset.x ?
+                parent.eulerAngles.x + Math.Clamp(transform.eulerAngles.x - parent.eulerAngles.x, -maxDirOffset.x, maxDirOffset.x)
+                : transform.eulerAngles.x
+            : transform.eulerAngles.x,
+            direction.y ?
+                maxDirOffset.y < 0 ?
+                parent.eulerAngles.y + dir.y
+                : transform.eulerAngles.y - parent.eulerAngles.y < -maxDirOffset.y || transform.eulerAngles.y - parent.eulerAngles.y > maxDirOffset.y ?
+                parent.eulerAngles.y + Math.Clamp(transform.eulerAngles.y - parent.eulerAngles.y, -maxDirOffset.y, maxDirOffset.y)
+                : transform.eulerAngles.y
+            : transform.eulerAngles.y,
+            direction.z ?
+                maxDirOffset.z < 0 ?
+                parent.eulerAngles.z + dir.z
+                : transform.eulerAngles.z - parent.eulerAngles.z < -maxDirOffset.z || transform.eulerAngles.z - parent.eulerAngles.z > maxDirOffset.z ?
+                parent.eulerAngles.z + Math.Clamp(transform.eulerAngles.z - parent.eulerAngles.z, -maxDirOffset.z, maxDirOffset.z)
+                : transform.eulerAngles.z
+            : transform.eulerAngles.z
+        );
 
     }
 }
