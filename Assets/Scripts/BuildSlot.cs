4963cec2ea0dca95dcb1b03ae36ce11355dab38f
a-rasines <alberto.rasines@opendeusto.es>
2023-03-09 15:54:38 +0100

    Arreglado el bug mas tonto que he tenido hasta el momento

--- Assets/Scripts/BuildSlot.cs
+++ Assets/Scripts/BuildSlot.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildSlot : Slot {
-    BuildManager manager;
-    public void OnInteraction() {
+    public BuildManager manager;
+    public new void OnInteraction() {
         Inventory.Stack s = inv.GetSlot(position);
         if (s.q == 0 || s.it.buildConstrain == ItemType.BuildConstrain.NonBuildable) return;
         manager.model = s.it.GetModel();
