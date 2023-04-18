a0ea800f2877ca32b2e0a77cf06e304608d136ff
a-rasines <alberto.rasines@opendeusto.es>
2023-04-18 08:39:06 +0200

    Pequeño arreglo de construcción

--- Assets/Scripts/Build/BuildSlot.cs
+++ Assets/Scripts/Build/BuildSlot.cs
@@ -8,7 +8,7 @@ public class BuildSlot : Slot {
         Inventory.Stack s = inv.GetSlot(position);
         if (s.q == 0 || s.it.buildConstrain == ItemType.BuildConstrain.NonBuildable) return;
         manager.model = s.it.GetModel();
-        manager.buildConstrain = ItemType.BuildConstrain.WaterBuildable;
+        manager.buildConstrain = s.it.buildConstrain;
         manager.enabled = true;
     }
 }
